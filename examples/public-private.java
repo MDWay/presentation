a4cb68834222eaf009a5996bd77ea82446962255
mdway <romangraef@gmail.com>
2017-12-22 16:41:12 +0100

    Now starting with the real content

---
+++ examples/public-private.java
@@ -0,0 +1,9 @@
+public class Haus {
+  b+publicb- void hausTuer() {
+    // Jeder kann zu meiner Haustür kommen. Deswegen ist es b+publicb-
+  }
+
+  b+privateb- void meinZimmer() {
+    // Aber in mein Zimmer das in meinem Haus liegt darf nur ich. Deswegen ist es b+privateb-
+  }
+}
